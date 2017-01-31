a413efc5916c9676864e3b5f7cc7bd39730238c7
Jeremy Herve <jeremy@tagada.hu>
2017-01-31 12:36:23 +0100

    Add uninstall cleanup.

---
+++ uninstall.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Cleaning up on uninstall.
+ *
+ * @package Traktivity
+ */
+
+if (
+	! defined( 'WP_UNINSTALL_PLUGIN' )
+	|| ! WP_UNINSTALL_PLUGIN
+	|| dirname( WP_UNINSTALL_PLUGIN ) != dirname( plugin_basename( __FILE__ ) )
+) {
+	exit;
+}
+
+// Delete option.
+delete_option( 'traktivity' );
+
+// Remove scheduled API calls.
+wp_clear_scheduled_hook( 'traktivity_publish' );
