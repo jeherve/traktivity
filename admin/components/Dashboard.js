c172012f92cceb32bb22aa9b3f7fc62dd375c4d4
Jeremy Herve <jeremy@tagada.hu>
2017-07-31 12:08:40 +0200

    Add recent events to Dashboard
    Add propTypes too.

--- admin/components/Dashboard.js
+++ admin/components/Dashboard.js
@@ -1,186 +1,119 @@
 /* External dependencies */
 import React from 'react';
+import WPAPI from 'wpapi';
+import PropTypes from 'prop-types';
 
 /* Internal dependencies */
-import Header from './Header';
-import Nav from './Nav.js';
-import Footer from './Footer';
-import TraktForm from './TraktForm';
-import TmdbForm from './TmdbForm';
 import Notice from './Notice';
+import routes from '../routes.json';
 
 class Dashboard extends React.Component {
 	constructor() {
 		super();
 
-		this.updateSettings = this.updateSettings.bind(this);
-		this.removeNotice = this.removeNotice.bind(this);
-		this.checkTraktCreds = this.checkTraktCreds.bind(this);
-		this.checkTmdbCreds = this.checkTmdbCreds.bind(this);
+		this.renderRecentEvents = this.renderRecentEvents.bind(this);
+		this.renderImage = this.renderImage.bind(this);
+
+		/* API Data */
+		this.site = new WPAPI({
+			endpoint: traktivity_dash.api_url,
+			routes: routes
+		});
 
 		// Initial state.
 		this.state = {
-			trakt: {
-				username: `${traktivity_dash.trakt_username}`,
-				key: `${traktivity_dash.trakt_key}`,
-			},
-			tmdb: {
-				key: `${traktivity_dash.tmdb_key}`,
-			},
-			notice: null,
-			samples: {},
+			recent : []
+		};
+	}
+
+	componentWillMount() {
+		// Launch sync if needed.
+		const syncStatus = this.props.sync;
+		if ( this.props.sync.pages != 0 ) {
+			this.props.launchSync();
+		}
+
+		// Check if we have some samples saved in sessionStorage.
+		const storedEvents = sessionStorage.getItem( 'traktivity_recent_events' );
+		// If not, let's query for a fresh recent event list and save it.
+		if ( ! storedEvents ) {
+			const recentEvents = this.site.traktivity_event()
+				.embed()
+				.param( 'per_page', 6 )
+				.then((data) => {
+					sessionStorage.setItem( 'traktivity_recent_events', JSON.stringify( data ) );
+					this.setState({ recent: data });
+				});
 		}
 	}
 
-	updateSettings( name, value ) {
-		// Get a copy of the existing state.
-		let settings = {...this.state};
+	componentDidMount() {
+		// Check if we have some samples saved in localStorage.
+		const recentEvents = sessionStorage.getItem( 'traktivity_recent_events' );
 
-		// Add in the data we got from the form.
-		if ( name === 'username' ) {
-			settings.trakt.username = value;
-		} else if ( name === 'key' ) {
-			settings.trakt.key = value;
-		} else if ( name === 'tmdb_key' ) {
-			settings.tmdb.key = value;
+		if ( recentEvents ) {
+			this.setState({ recent: JSON.parse(recentEvents) });
 		}
+	}
 
-		// Save our state.
-		this.setState({
-			trakt: settings.trakt,
-			tmdb: settings.tmdb
+	renderRecentEvents() {
+		const events = this.state.recent;
+
+		const list = events.map(event => {
+			return (
+				<a href={event.link} title={event.title.rendered} className="event-link" key={event.id}>
+					<img
+						className="event-image"
+						src={event._embedded['wp:featuredmedia'][0].media_details.sizes.medium.source_url}
+						alt={event._embedded['wp:featuredmedia'][0].alt_text}
+						width={event._embedded['wp:featuredmedia'][0].media_details.sizes.medium.width}
+						height={event._embedded['wp:featuredmedia'][0].media_details.sizes.medium.height}
+					/>
+				<span className="event-title">{event.title.rendered}</span>
+				</a>
+			)
 		});
 
-		// Save in db as well.
-		const postOptions = {
-			credentials: 'same-origin',
-			method: 'POST',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json',
-				'X-WP-Nonce': traktivity_dash.api_nonce
-			},
-			body: JSON.stringify(
-				settings
-			),
-		};
-		const settingsPromise = fetch( `${traktivity_dash.api_url}/traktivity/v1/settings/edit`, postOptions );
-		return settingsPromise
-			.then((response) => {
-				if (response.status === 200 ) {
-					this.setState({ notice: {
-						message: traktivity_dash.notice_saved,
-						type: 'success',
-					}});
-				} else {
-					this.setState({ notice: {
-						message: traktivity_dash.notice_error,
-						type: 'error',
-					}});
-				}
-			})
-			.then((response) => {
-				if ( settings.trakt.username && settings.trakt.key ) {
-					this.checkTraktCreds(settings.trakt.username, settings.trakt.key);
-				}
-			})
-			.then((response) => {
-				if ( settings.tmdb.key ) {
-					this.checkTmdbCreds(settings.tmdb.key);
-				}
-			})
-			.catch((err) => {
-				this.setState({ notice: {
-					message: `${err}`,
-					type: 'error',
-				}});
-			});
+		return (
+			<div className="card event-list">
+				<h3 className="list-title">{traktivity_dash.recent_list_title}</h3>
+				<div className="images">{list}</div>
+			</div>
+		)
 	}
 
-	checkTraktCreds( username, key ) {
-		const fetchOptions = {
-			credentials: 'same-origin',
-			method: 'GET',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json',
-				'X-WP-Nonce': traktivity_dash.api_nonce
-			},
-		};
-		const checkCredsPromise = fetch( `${traktivity_dash.api_url}/traktivity/v1/connection/${username}/${key}`, fetchOptions );
-		return checkCredsPromise
-			.then((response) => response.json())
-			.then((body) => {
-				this.setState({ notice: {
-					message: body.message,
-					type: `${body.code === 200 ? 'success' : 'error'}`,
-				}});
-			})
-			.catch((err) => {
-				this.setState({ notice: {
-					message: `${err}`,
-					type: 'error',
-				}});
-			});
-	}
-
-	checkTmdbCreds( key ) {
-		const fetchOptions = {
-			credentials: 'same-origin',
-			method: 'GET',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json',
-				'X-WP-Nonce': traktivity_dash.api_nonce
-			},
-		};
-		const checkCredsPromise = fetch( `${traktivity_dash.api_url}/traktivity/v1/tmdb/${key}`, fetchOptions );
-		return checkCredsPromise
-			.then((response) => response.json())
-			.then((body) => {
-				this.setState({
-					notice: {
-						message: body.message,
-						type: `${body.code === 200 ? 'success' : 'error'}`,
-					},
-					samples: {...body.samples}
-				});
-			})
-			.catch((err) => {
-				this.setState({ notice: {
-					message: `${err}`,
-					type: 'error',
-				}});
-			});
-	}
-
-	removeNotice() {
-		this.setState({ notice: null });
+	renderImage(event) {
+		return null;
 	}
 
 	render() {
 		return (
 			<div className="traktivity_dashboard">
-				<Header />
-				<Nav />
 				<Notice
-					notice={this.state.notice}
-					removeNotice={this.removeNotice}
+					notice={this.props.notice}
+					removeNotice={this.props.removeNotice}
 				/>
 				<div className="card_list">
-					<TraktForm
-						trakt={this.state.trakt}
-						updateSettings={this.updateSettings}
-					/>
-					<TmdbForm
-						tmdb={this.state.tmdb}
-						updateSettings={this.updateSettings}
-					/>
+					<div className="card faq">
+						<p><strong>{traktivity_dash.dashboard_intro_q}</strong></p>
+						<p>{traktivity_dash.dashboard_intro_a}</p>
+						<p><strong>{traktivity_dash.dashboard_sup_trakt_q}</strong></p>
+						<p>{traktivity_dash.dashboard_sup_trakt_a}</p>
+						<p><strong>{traktivity_dash.dash_faq_who}</strong></p>
+						<p>{traktivity_dash.trakt_dash_credits}</p>
+					</div>
+					{this.renderRecentEvents()}
 				</div>
-				<Footer />
 			</div>
 		)
 	}
 }
+
+Dashboard.propTypes = {
+	sync: PropTypes.object.isRequired,
+	launchSync: PropTypes.func.isRequired,
+	notice: PropTypes.object,
+	removeNotice: PropTypes.func.isRequired,
+};
 
 export default Dashboard;
