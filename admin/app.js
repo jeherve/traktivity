589cc37dbc130b5a335f02c5c9db06eb9eacb45e
Jeremy Herve <jeremy@tagada.hu>
2017-07-31 12:03:10 +0200

    Routes will remain commented out for now.

--- admin/app.js
+++ admin/app.js
@@ -5,15 +5,16 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 /* Internal dependencies */
 import Setup from './components/Setup';
+import Dashboard from './components/Dashboard';
 import Parameters from './components/Parameters';
 import Faq from './components/Faq';
 
 render((
 	<BrowserRouter>
 		<Switch>
-			// <Route exact path='/' component={ Dashboard }/>
-			// <Route path='/parameters' component={ Parameters }/>
-			// <Route path='/faq' component={ Faq }/>
+			// <Route exact path='/' component={ Setup }/>
+			// <Route path={`${traktivity_dash.dash_url}/parameters`} component={ Parameters }/>
+			// <Route path={`${traktivity_dash.dash_url}/faq`} component={ Faq }/>
 			<Route path="*" component={ Setup } />
 		</Switch>
 	</BrowserRouter>
